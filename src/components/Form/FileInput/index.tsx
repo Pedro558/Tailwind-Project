d4053e6875caada31d0058d700a9506584bde608
pedro558 <pedroafonso109@gmail.com>
2026-01-18 21:51:39 -0300

    :sparkles: feat: form finished

---
+++ src/components/Form/FileInput/index.tsx
@@ -0,0 +1,7 @@
+import { Trigger } from "./Trigger";
+import { ImagePreview } from "./ImagePreview"
+import { Control } from "./Control";
+import { Root } from "./Root";
+import { FileList } from "./FileList";
+
+export { Trigger, ImagePreview, Control, Root, FileList }
